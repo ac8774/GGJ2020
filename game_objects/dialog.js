3caf432228a20b044b67dd3ebc6854efc0d9553c
Andrew Clark <ac8774@gmail.com>
2020-02-02 03:52:29 -0600

    dialog slide in

--- game_objects/dialog.js
+++ game_objects/dialog.js
@@ -5,7 +5,9 @@ class Dialog{
 
         //Coordinates of top-left corner
         this.x=30;
-        this.y=30;
+        this.y=600;
+
+        this.targety=30;
 
         //Initialize pop-up animation
         this.shadow=10;
@@ -15,26 +17,28 @@ class Dialog{
     }
 
     render(){
+        this.y -= Math.sign(this.y - this.targety)*Math.pow(Math.abs(this.y - this.targety), 0.8) * 0.25;
+        if(Math.abs(this.y-this.targety)<1) this.y = this.targety;
         ctx.save();
         ctx.translate(this.x,this.y);
-        
+
         drawImageShadow("dialog_background",0,0,this.shadow);
-        
+
         drawImage("dialog_title_background",
                   sprites["dialog_background"].width/2-
                       sprites["dialog_title_background"].width/2,
                   10,
                   1);
-        
+
         ctx.textAlign = "center";
         ctx.font = "30px Arial Black";
         ctx.fillStyle = "#8a532e";
         ctx.fillText(this.title,
                      sprites["dialog_background"].width/2,
                      46);
-        
+
         this.children.forEach(btn => btn.render());
-        
+
         ctx.restore();
     }
 
