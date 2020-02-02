3fccd2b0e9fcd6a482e0732503074a5fee54d6ff
dandarnell <dandarnell@users.noreply.github.com>
2020-02-01 22:11:22 -0600

    Update status_bar.js

---
+++ game_objects/status_bar.js
@@ -0,0 +1,12 @@
+class StatusBar{
+    constructor(){
+        
+    }
+    
+    render(){
+        ctx.beginPath();
+        ctx.fillStyle="black";
+        ctx.rect(0,448,640,32);
+        ctx.fill();
+    }
+}
