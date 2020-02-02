ac5cbb4922cc8de987f57ce7dca85f3fcaf45753
Andrew Clark <ac8774@gmail.com>
2020-02-02 06:06:53 -0600

    .

--- game_objects/ingredient_viewer.js
+++ game_objects/ingredient_viewer.js
@@ -2,7 +2,7 @@ class IngredientViewer extends Button{
     constructor(x,y,i){
         super(x,y,function(){
             var r = recipeList();
-            if(this.mousex>130){
+            if(this.mousex>140){
                 delete recipe[r[this.i]];
             }else{
                 activeIngredient = r[this.i];
