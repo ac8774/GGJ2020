42408d24843d3f6804565e84ef661f3e11b849f2
dandarnell <dandarnell@users.noreply.github.com>
2020-02-02 02:07:04 -0600

    Updated status bar

--- game_objects/status_bar.js
+++ game_objects/status_bar.js
@@ -1,6 +1,9 @@
 class StatusBar{
     constructor(){
-        
+        this.nextTurnButton=new IconButton("ic/next_turn",
+                                           584,
+                                           8,
+                                           function(){});
     }
     
     render(){
@@ -8,5 +11,12 @@ class StatusBar{
         ctx.fillStyle="black";
         ctx.rect(0,416,640,64);
         ctx.fill();
+        
+        ctx.save();
+        ctx.translate(0,416);
+        
+        this.nextTurnButton.render();
+        
+        ctx.restore();
     }
 }
